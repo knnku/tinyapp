58a40ea8f0d318ac0215880510a469f5da97ef58
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-21 03:15:46 +0000

    Form submit and save to db added. Modified some buttons and added external longurl access route.

--- express_server.js
+++ express_server.js
@@ -1,29 +1,39 @@
 const express = require("express");
-const tinyApp = express();
+const tinyUrlApp = express();
 const PORT = 8080;
 
 function generateRandomString() {
-  return Math.random().toString(36).substring(3,9);
-};
+  return Math.random().toString(36).substring(3, 9);
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-tinyApp.set("view engine", "ejs");
-tinyApp.use(express.urlencoded({ extended: true }));
+tinyUrlApp.set("view engine", "ejs");
+tinyUrlApp.use(express.urlencoded({ extended: true }));
 
-tinyApp.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+tinyUrlApp.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
-tinyApp.get("/urls/new", (req, res) => {
+tinyUrlApp.post("/urls", (req, res) => {
+  // console.log(req.body); // Log the POST request body to the console
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let tinyUrl = generateRandomString();
+  let longUrl = req.body.longURL;
+  urlDatabase[tinyUrl] = `http://www.${longUrl}`; 
+
+  res.redirect(`/urls/${tinyUrl}`);
+});
+
+tinyUrlApp.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-tinyApp.get("/urls/:id", (req, res) => {
+tinyUrlApp.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
@@ -31,24 +41,23 @@ tinyApp.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-tinyApp.get("/urls", function (req, res) {
+tinyUrlApp.get("/urls", function (req, res) {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-tinyApp.get("/", function (req, res) {
+tinyUrlApp.get("/", function (req, res) {
   res.send("Hello!");
 });
 
-tinyApp.get("/urls.json", function (req, res) {
+tinyUrlApp.get("/urls.json", function (req, res) {
   res.json(urlDatabase);
 });
 
-tinyApp.get("/hello", function (req, res) {
+tinyUrlApp.get("/hello", function (req, res) {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-tinyApp.listen(PORT, () => {
+tinyUrlApp.listen(PORT, () => {
   console.log(`The server is listening on port: ${PORT}`);
-  console.log(generateRandomString());
 });

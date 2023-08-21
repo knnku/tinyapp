496c7ca65c0f6c3791e0d5cdcb77784f1985f56e
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-21 02:19:55 +0000

    implement function to generate random string

--- express_server.js
+++ express_server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const tinyApp = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(3,9);
+};
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 tinyApp.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+tinyApp.use(express.urlencoded({ extended: true }));
+
+tinyApp.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 tinyApp.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -41,4 +50,5 @@ tinyApp.get("/hello", function (req, res) {
 
 tinyApp.listen(PORT, () => {
   console.log(`The server is listening on port: ${PORT}`);
+  console.log(generateRandomString());
 });
